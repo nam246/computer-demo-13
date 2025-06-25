d29d26279e7bd6d88ad5965eccc6c61971037812
nam246 <nam24620@gmail.com>
2025-06-25 15:48:20 +0700

    fixed bug

--- app/(home)/[slug]/page.tsx
+++ app/(home)/[slug]/page.tsx
@@ -1,24 +1,3 @@
-import prisma from "@/lib/prisma";
-type PageProps = {
-	params: { slug: string };
-};
-
-export default async function Pages({ params }: PageProps) {
-	const page = await prisma.page.findFirst({
-		where: {
-			slug: params.slug,
-		},
-	});
-
-	if (page) {
-		return (
-			<div className="container mx-auto">
-				<h1>{page?.title}</h1>
-				<div>{page?.description}</div>
-				<div>{page?.content}</div>
-			</div>
-		);
-	} else {
-		return <div className="text-9xl">Not found</div>;
-	}
+export default async function Pages({ params }: { params: { slug: string } }) {
+	return <h1>{params.slug}</h1>;
 }
